8bba2b8da98e92a702c56cf4a390627a52b01cdf
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2022-04-19 17:42:39 +0100

    Added Main method and renamed Program.cs

---
+++ Shitfaced.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace shitfaced
+{
+   public class Shitfaced
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Starting!");
+
+            // Stop the console from immediately closing
+            Console.WriteLine("Shitfaced has finished executing! Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
