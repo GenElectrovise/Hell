3b51e464c202f7ae40aefe0d6b7e73e24b99a328
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2022-04-23 12:33:22 +0100

    Deleted Compiler. Interpreter might now copy new files to the runningfile and start to index the running file. Or it also might not because I've tested none of this.

--- src/interpreter/Interpreter.cs
+++ src/interpreter/Interpreter.cs
@@ -1,4 +1,6 @@
-﻿using Hell.Compilation;
+﻿using Hell.src.interpreter;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Hell.Interpretation
 {
@@ -16,26 +18,34 @@ namespace Hell.Interpretation
     internal class Interpreter
     {
         public RunContext RunContext { get; }
-        public Compiler Compiler { get; set; }
         private int _programCounter = 0;
+        private string _runningFilePath = "";
+        private int _runningFileLength = 0;
 
         public Interpreter(RunContext runContext)
         {
             this.RunContext = runContext;
-            Compiler = new Compiler(this.RunContext);
         }
 
         /// <summary>
-        /// Prepares the interpreter environment to start executing by, for example, compiling the root file.
+        /// Prepares the interpreter environment to start executing by, for example, finding the root file.
         /// </summary>
         /// <returns>Whether the preparation was successful, and the program can start to execute</returns>
         internal Boolean PrepareToStart()
         {
-            Boolean rootFileCompiles = Compiler.CompileFromFile(RunContext.S_ProgramPath);
-            _programCounter = Compiler.EntryLine;
-
-            if (!rootFileCompiles)
+            if (!File.Exists(RunContext.S_ProgramPath))
+            {
+                Hell.Info("Ah darn it, that program's not where you said... Guess I'll just make one up!");
+                Hell.Debug("File " + RunContext.S_ProgramPath + " does not exist. Creating...");
                 return false;
+            }
+
+            // Create the runnningfile
+            _runningFilePath = RunContext.S_ProgramPath + ".runningfile";
+            File.Open(_runningFilePath, FileMode.OpenOrCreate);
+
+            AddNewFile(RunContext.S_ProgramPath);
+
             return true;
         }
 
@@ -44,6 +54,7 @@ namespace Hell.Interpretation
         internal bool HasNextLine()
         {
             //throw new NotImplementedException();
+
             i++;
             return i<20;
         }
@@ -52,6 +63,63 @@ namespace Hell.Interpretation
         {
             //throw new NotImplementedException();
             Console.WriteLine(i);
+        }
+
+        internal void AddNewFile(string path)
+        {
+            CopyNewFileIntoRunningFile(path);
+            IndexRunningFile();
+        }
+
+        internal void CopyNewFileIntoRunningFile(string path)
+        {
+            byte[] newLines = Encoding.UTF8.GetBytes("\n\n");
+            byte[] contents = File.ReadAllBytes(path);
+            byte[] combination = new byte[newLines.Length + contents.Length];
+            Buffer.BlockCopy(newLines, 0, combination, 0, newLines.Length);
+            Buffer.BlockCopy(contents, 0, combination, newLines.Length, contents.Length);
+
+            FileStream stream = File.Open(_runningFilePath, FileMode.Append);
+            stream.Write(combination, 0, combination.Length);
+        }
+
+        private void IndexRunningFile()
+        {
+            _runningFileLength = GetLineCount(_runningFilePath);
+
+            // Find function declarations
+            // i ranges 1 to rFL
+            for (int i = 1; i <= _runningFileLength; i++)
+            {
+                // If the current line starts a paragraph
+                string line = ReadLine(_runningFilePath, i);
+                if (line.StartsWith(Tokens.PARAGRAPH_START))
+                {
+                    int lineNumber = i;
+                    string pureLine = line.Replace(Tokens.ENTRY_PARAGRAPH_NAME, "");
+
+                    foreach (char c in pureLine.ToCharArray())
+                    {
+                        // TODO Form the name of the method and store to running file index
+                    }
+                }
+            }
+        }
+
+        internal int GetLineCount(string path)
+        {
+            return File.ReadLines(path).Count();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="lineNumber">Number greater than 0. The line numbering starts at 1.</param>
+        /// <returns></returns>
+        internal string ReadLine(string path, int lineNumber)
+        {
+            return File.ReadLines(path).Skip(lineNumber - 1).Take(1).First();
         }
     }
 }
---
+++ src/interpreter/Tokens.cs
@@ -0,0 +1,8 @@
+﻿namespace Hell.src.interpreter
+{
+    internal class Tokens
+    {
+        public static readonly string ENTRY_PARAGRAPH_NAME = "once-upon-a-time-there-was-a-program";
+        public static readonly string PARAGRAPH_START = "\t";
+    }
+}
