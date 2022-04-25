59e45f87cb531707f2aeab9bc2d5a48b47aae90e
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2022-04-25 21:36:03 +0100

    Added Tokens documenation to the language spec and did some suspicious things with Token classes...

---
+++ src/interpreter/token/ResultOfTokens.cs
@@ -0,0 +1,23 @@
+﻿namespace Hell.src.interpreter.token
+{
+    internal class ResultOfToken : IToken
+    {
+        public static readonly string RESULT_OF_TOKEN = "the result of";
+        public IToken? Target { get; set; }
+
+        internal ResultOfToken()
+        {
+        }
+
+        string[] IToken.Resolve(string[] arguments)
+        {
+            this.Target = arguments[0];
+            return arguments.Where((content, index) => { return index != 0; }).ToArray();
+        }
+
+        public override string ToString()
+        {
+            return "with " + Target;
+        }
+    }
+}
--- src/interpreter/token/RunToken.cs
+++ src/interpreter/token/RunToken.cs
@@ -3,6 +3,7 @@
     internal class RunToken : IToken
     {
         public string? FunctionName { get; set; }
+        public IToken? Target { get; set; }
 
         internal RunToken()
         {
@@ -10,6 +11,18 @@
 
         string[] IToken.Resolve(string[] arguments)
         {
+            // If "the result of"
+            if (arguments.Length >= 3)
+            {
+                if (arguments[0].Equals("the") && arguments[1].Equals("result") && arguments[2].Equals("of"))
+                {
+                    arguments = arguments.Where((content, index) => { return index <= 2; }).ToArray(); // Remove "the result of"
+                    TokenFactory.make(ResultOfToken.RESULT_OF_TOKEN).Resolve(arguments);
+                }
+            }
+
+            // If a paragraph name
+
             this.FunctionName = arguments[0];
             return arguments.Where((content, index) => { return index != 0; }).ToArray();
         }
